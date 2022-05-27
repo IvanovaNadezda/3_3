e1b4b51a7f42ff5b89e404d36c7b51eb7ffdb595
inade <inadezda2003@gmail.com>
2022-05-28 02:17:13 +0300

    Initial commit

--- Application.h
+++ Application.h
@@ -15,38 +15,31 @@
 using namespace std;
 class Application : public Base_class {
 private:
-    int class_number = 1; //номер класса
-    Base_class* parent; // Ссылки на объект класса-родителя и объект дочернего класса
-    string parent_name, child_name; //Наименование объектов класса - родителя и дочернего класса соответственно
-    string url; //Путь до оюъекта
+    int class_number = 1;
+    Base_class* parent;
+    string parent_name, child_name;
+    string url;
 public:
 
-    //Параметрический конструктор
+
     Application(Base_class* parent = NULL, string name =
     "Root_obj");
 
-    //Построить дерево объектов
     void built_tree_objs();
 
-    //Метод сигнала
+
     void signal(string& message);
 
-    //Обработчик
+
     void handler(string message);
 
-    //Установка связей между объектами
+
     void set_tree_connects();
-
-    //Обработка команд пользователя(SET_CONNECT, DELETE_CONNECT, EMIT, SET_CONDITION)
     void tree_connect_commands();
-
-    //Установить состояние объектов дерева
     void set_tree_condition();
 
-    //Функция для обработки команд SET и FIND
     void tree_commands();
 
-    //Вывод дерева иерархии
     int exec();
 };
 #endif
