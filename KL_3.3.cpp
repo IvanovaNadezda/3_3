10de64cd7d480082ced85e5352867a9f65f04873
inade <inadezda2003@gmail.com>
2022-05-27 23:11:52 +0300

    Initial commit

---
+++ KL_3.3.cpp
@@ -0,0 +1,14 @@
+//
+// Created by inade on 27.05.2022.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "Application.h"
+int main()
+{
+    Application* app = new Application(); //Создаем объкт класса Application
+    app->built_tree_objs();//Строим иерархическое древо
+    app->set_tree_connects();//Обрабатываем команды пользователя
+    return app->exec(); // Выводим иерархическое древо на экран
+}
